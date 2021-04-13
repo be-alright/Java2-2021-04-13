bd6c4a2a29e45db769504b037e05ee6542154963
cindy <cindychan9256@gmail.com>
2021-04-13 16:22:41 +0800

    add commit

--- src/main/java/com/cindy/Racing.java
+++ src/main/java/com/cindy/Racing.java
@@ -5,8 +5,12 @@ public class Racing {
 
         Horse h1 = new Horse();
         Horse h2 = new Horse();
+        HorseRunnable h3 = new HorseRunnable();
+        Thread thread = new Thread(h3);
+        thread.start();
         h1.start();
         h2.start();
+
         //for (int i = 0; i < 10000; i++) {
         //System.out.println(name1 +  " " + i);
     }
