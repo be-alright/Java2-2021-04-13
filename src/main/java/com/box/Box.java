b12eecfc5e0152204c4353c8b5e64371d63880c7
cindy <cindychan9256@gmail.com>
2021-04-27 15:17:21 +0800

    initial commit

---
+++ src/main/java/com/box/Box.java
@@ -0,0 +1,17 @@
+package com.box;
+
+public class Box {
+    int length;
+    int width;
+    int height;
+
+    public boolean validate(int a, int b, int c) {
+        if (length >= a && width >= b && height >= c) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+}
+
---
+++ src/main/java/com/box/Box2.java
@@ -0,0 +1,10 @@
+package com.box;
+
+public class Box2 extends Box {
+    public Box2() {
+        length = 15;
+        width = 10;
+        height = 10;
+    }
+
+}
