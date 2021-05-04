5e54bbc0622c457798558dfcf911858d9938ab80
cindy <cindychan9256@gmail.com>
2021-05-04 16:26:31 +0800

    5/4 add commit

--- src/main/java/com/box/collection/Collection.java
+++ src/main/java/com/box/collection/Collection.java
@@ -1,6 +1,8 @@
 package com.box.collection;
 
+import com.box.Box;
 import com.box.Box1;
+import com.box.Box2;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -9,12 +11,21 @@ public class Collection {
     public static void main(String[] args) {
 
         ArrayList list = new ArrayList();
-        String s = new String("name");
-        list.add(s);
+        //String s = new String("cindy");
+        list.add("cindy");
         Integer n = new Integer(123);
         list.add(n);
         list.add(true);
+        Box1 box = new Box1();
+        list.add(box);
+        //
+        String name = (String) list.get(0);
+        String abc = (String) list.get(1);
+        //
         Box1 box1 = new Box1();
-        list.add(box1);
+        Box2 box2 = new Box2();
+        ArrayList<Box> boxes = new ArrayList<Box>();
+        boxes.add(box1);
+        boxes.add(box2);
     }
 }
